01a58996457373bf2fe4098900d6360212e9e748
Emile <latour@ohsu.edu>
2019-03-23 14:46:50 -0700

    display logistic, helpers, other package stuff

---
+++ R/adorn-reg-table-format.R
@@ -0,0 +1,358 @@
+
+#' @title
+#' Add formatting to a table of regression results
+#'
+#' @description
+#' Add formatting to a table of regression results....
+#'
+#' @param tab A regression table. Data frame or tibble
+#' @param digits Integer; number of digits to display (default is 2)
+#' @param pval_fmt Which p-value to show. Options are (1) "combo", default,
+#'   which shows the LRT and Wald but only if there is more than one level, (2)
+#'   "all" shows LRT and Wald no matter what, (3) "LRT" shows just the LRT
+#'   p-value, (4) "Wald" shows just the Wald p-value.
+#' @param format Doesn't do anything right now. Place holder for later. Default
+#'   is "html".
+#' @param separate_text Text to use in confidence intervals. Default is " to ".
+#' @param var_labels Variable labels
+#'
+#' @importFrom dplyr case_when
+#' @importFrom dplyr coalesce
+#' @importFrom dplyr if_else
+#' @importFrom dplyr mutate
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr mutate_at
+#' @importFrom dplyr select
+#' @importFrom labelled var_label
+#'
+#' @return A tibble or data frame
+#' @export
+#'
+#' @examples \dontrun{
+#' library(epiDisplay)
+#' library(tidyverse)
+#'
+#' dplyr::glimpse(infert)
+#' model0 <- glm(case ~ induced + spontaneous + education,
+#'               family = binomial,
+#'               data = infert)
+#'
+#' tab1 <- display_logistic(fit = model0,
+#'                  data = infert)
+#'
+#' adorn_reg_table_format(tab1,
+#'                          pval_fmt = "all")
+#'
+#' adorn_reg_table_format(tab1,
+#'                          pval_fmt = "combo")
+#'
+#'
+#' adorn_reg_table_format(tab1,
+#'                          pval_fmt = "combo")
+#'
+#' tab2 <- display_logistic(fit = model0,
+#'                          data = infert,
+#'                          add_multi = TRUE)
+#'
+#' adorn_reg_table_format(tab2,
+#'                          pval_fmt = "combo")
+#'
+#'
+#'
+#' # How does an interaction term work?
+#' # This sort of works right now, but still needs work (2019-03-23)
+#' # Does't work very well with the multivariable results
+#' model0 <- glm(case ~ induced * education,
+#'               family = binomial,
+#'               data = infert)
+#'
+#' tab3 <- display_logistic(fit = model0,
+#'                  data = infert,
+#'                  add_multi = FALSE)
+#'
+#' adorn_reg_table_format(tab3,
+#'                          pval_fmt = "all")
+#'
+#' display_logistic(fit = model0,
+#'                  data = infert,
+#'                  add_multi = TRUE) %>%
+#'   adorn_reg_table_format(.,
+#'                          pval_fmt = "combo") %>%
+#'   print(n = Inf)
+#'
+#'
+#'
+#' ## With labels ----------------
+#'
+#' model0 <- glm(case ~ induced + spontaneous + education,
+#'               family = binomial,
+#'               data = infert)
+#'
+#' tab4 <- display_logistic(fit = model0,
+#'                  data = infert,
+#'                  add_multi = TRUE)
+#'
+#' adorn_reg_table_format(tab4,
+#'                          pval_fmt = "combo",
+#'                          var_labels = list(induced = "Induced (1/0)",
+#'                                            spontaneous = "Spontaneous (1/0)",
+#'                                            education = "Education levels"))
+#'
+#'
+#' # Another way if the data set is already labelled
+#'
+#' labelled::var_label(infert) <- list(induced = "Induced (1/0)",
+#'                                     spontaneous = "Spontaneous (1/0)",
+#'                                     education = "Education levels")
+#'
+#' adorn_reg_table_format(tab4,
+#'                          pval_fmt = "combo",
+#'                          var_labels = labelled::var_label(infert))
+#' }
+
+adorn_reg_table_format <- function(tab,
+                                   digits = 2,
+                                   pval_fmt = "combo",  # "lrt" "wald" "all"
+                                   format = "html",
+                                   separate_text = " to ",
+                                   var_labels = NULL) {
+
+  if (length(names(tab)) == 9) {
+
+
+    ## Combine/select p-values ----------------
+
+    if (pval_fmt == "combo") {
+
+      tab <- tab %>%
+        mutate(p_value_wald = dplyr::case_when(
+          is.na(ref) ~ p_value_wald,
+          ref == "Reference level" ~ p_value_wald,
+          ref == "" ~ NA_real_),
+          p_value = dplyr::coalesce(p_value_wald,
+                                    p_value_lrt))
+
+    } else if (pval_fmt == "lrt") {
+
+      tab <- tab %>%
+        mutate(p_value = p_value_lrt)
+
+    } else if (pval_fmt == "wald") {
+
+      tab <- tab %>%
+        mutate(p_value = p_value_wald)
+
+    } else if (pval_fmt == "all") {
+
+      tab <- tab %>%
+        mutate(p_value = dplyr::coalesce(p_value_wald,
+                                         p_value_lrt))
+
+    }
+
+    ## Recalc signif indicator ----------------
+
+    tab <- tab %>%
+      mutate(signif = .calc_sig_ind(p_value,
+                                    format = format))
+
+    ## Format p-value ----------------
+
+    tab <- tab %>%
+      mutate(p_value = .mypval(p_value))
+
+
+    ## Combine estimate and CI ----------------
+
+    tab <- tab %>%
+      mutate(est_is_na = dplyr::if_else(is.na(estimate),
+                                        TRUE,
+                                        FALSE)) %>%
+      mutate_at(.vars = vars(estimate, lower_ci, upper_ci),
+                .funs = list ~ .myround(.,
+                                        digits = digits)) %>%
+      mutate(est_ci = paste0(estimate, " (",
+                             lower_ci, separate_text, upper_ci, ")"),
+             est_ci = dplyr::if_else(est_is_na,
+                                     NA_character_,
+                                     est_ci)) %>%
+      mutate(est_ci = dplyr::coalesce(est_ci,
+                                      ref))
+
+
+    ## Select columns and replace NAs ----------------
+
+    tab <- tab %>%
+      dplyr::select(covariate,
+                    term,
+                    est_ci,
+                    p_value,
+                    signif) %>%
+      mutate_all(.,
+                 list(~ dplyr::if_else(is.na(.), "", .)))
+
+
+  } else if (length(names(tab)) == 15) {
+
+
+    ## Combine/select p-values ----------------
+
+    # Univariable
+
+    if (pval_fmt == "combo") {
+
+      tab <- tab %>%
+        mutate(p_value_wald_unadjusted = dplyr::case_when(
+          is.na(ref) ~ p_value_wald_unadjusted,
+          ref == "Reference level" ~ p_value_wald_unadjusted,
+          ref == "" ~ NA_real_),
+          p_value_unadjusted = dplyr::coalesce(p_value_wald_unadjusted,
+                                               p_value_lrt_unadjusted))
+
+    } else if (pval_fmt == "lrt") {
+
+      tab <- tab %>%
+        mutate(p_value_unadjusted = p_value_lrt_unadjusted)
+
+    } else if (pval_fmt == "wald") {
+
+      tab <- tab %>%
+        mutate(p_value_unadjusted = p_value_wald_unadjusted)
+
+    } else if (pval_fmt == "all") {
+
+      tab <- tab %>%
+        mutate(p_value_unadjusted = dplyr::coalesce(p_value_wald_unadjusted,
+                                                    p_value_lrt_unadjusted))
+
+    }
+
+
+    # Multivariable
+
+    if (pval_fmt == "combo") {
+
+      tab <- tab %>%
+        mutate(p_value_wald_adjusted = dplyr::case_when(
+          is.na(ref) ~ p_value_wald_adjusted,
+          ref == "Reference level" ~ p_value_wald_adjusted,
+          ref == "" ~ NA_real_),
+          p_value_adjusted = dplyr::coalesce(p_value_wald_adjusted,
+                                             p_value_lrt_adjusted))
+
+    } else if (pval_fmt == "lrt") {
+
+      tab <- tab %>%
+        mutate(p_value_adjusted = p_value_lrt_adjusted)
+
+    } else if (pval_fmt == "wald") {
+
+      tab <- tab %>%
+        mutate(p_value_adjusted = p_value_wald_adjusted)
+
+    } else if (pval_fmt == "all") {
+
+      tab <- tab %>%
+        mutate(p_value_adjusted = dplyr::coalesce(p_value_wald_adjusted,
+                                                  p_value_lrt_adjusted))
+
+    }
+
+
+
+
+    ## Recalc signif indicator ----------------
+
+    tab <- tab %>%
+      mutate(signif_unadjusted = .calc_sig_ind(p_value_unadjusted,
+                                               format = format),
+             signif_adjusted = .calc_sig_ind(p_value_adjusted,
+                                             format = format))
+
+    ## Format p-value ----------------
+
+    tab <- tab %>%
+      mutate(p_value_unadjusted = .mypval(p_value_unadjusted),
+             p_value_adjusted = .mypval(p_value_adjusted))
+
+
+    ## Combine estimate and CI ----------------
+
+    # Univariable
+    tab <- tab %>%
+      mutate(est_is_na_unadjusted = dplyr::if_else(is.na(estimate_unadjusted),
+                                                   TRUE,
+                                                   FALSE)) %>%
+      mutate_at(.vars = vars(estimate_unadjusted,
+                             lower_ci_unadjusted,
+                             upper_ci_unadjusted),
+                .funs = list ~ .myround(.,
+                                        digits = digits)) %>%
+      mutate(est_ci_unadjusted = paste0(estimate_unadjusted, " (",
+                                        lower_ci_unadjusted, separate_text,
+                                        upper_ci_unadjusted, ")"),
+             est_ci_unadjusted = dplyr::if_else(est_is_na_unadjusted,
+                                                NA_character_,
+                                                est_ci_unadjusted)) %>%
+      mutate(est_ci_unadjusted = dplyr::coalesce(est_ci_unadjusted,
+                                                 ref))
+
+    # Multivariable
+    tab <- tab %>%
+      mutate(est_is_na_adjusted = dplyr::if_else(is.na(estimate_adjusted),
+                                                 TRUE,
+                                                 FALSE)) %>%
+      mutate_at(.vars = vars(estimate_adjusted,
+                             lower_ci_adjusted,
+                             upper_ci_adjusted),
+                .funs = list ~ .myround(.,
+                                        digits = digits)) %>%
+      mutate(est_ci_adjusted = paste0(estimate_adjusted, " (",
+                                      lower_ci_adjusted, separate_text,
+                                      upper_ci_adjusted, ")"),
+             est_ci_adjusted = dplyr::if_else(est_is_na_adjusted,
+                                              NA_character_,
+                                              est_ci_adjusted)) %>%
+      mutate(est_ci_adjusted = dplyr::coalesce(est_ci_adjusted,
+                                               ref))
+
+
+    ## Select columns and replace NAs ----------------
+
+    tab <- tab %>%
+      dplyr::select(covariate,
+                    term,
+                    est_ci_unadjusted,
+                    p_value_unadjusted,
+                    signif_unadjusted,
+                    est_ci_adjusted,
+                    p_value_adjusted,
+                    signif_adjusted) %>%
+      mutate_all(.,
+                 list(~ dplyr::if_else(is.na(.), "", .)))
+
+
+  }
+
+
+  ## Apply labels ----------------
+
+  if (!is.null(var_labels)) {
+
+    var_labels <- Filter(Negate(is.null), var_labels)
+
+    for (i in 1:length(var_labels)) {
+
+      tab$covariate <- gsub(paste0("^", names(var_labels[i])),
+                            var_labels[[i]],
+                            tab$covariate)
+    }
+
+  }
+
+  ## Return table ----------------
+
+  tab
+
+
+}
---
+++ R/display-logistic2.R
@@ -0,0 +1,258 @@
+#' @title
+#' Display logistic regression results (given variable names)
+#'
+#' @description
+#' Given a string with an "outcome" and a vector string with "predictors",
+#' univariable and (optionally) multivariable results from logistic regression
+#' are displayed in a format suitable for presentation.
+#'
+#' This version allows you to explicitly provide the outcome and predictors in
+#' case there are issues with getting a full model fit.
+#'
+#' @param data A tibble or data frame with the full data set.
+#' @param outcome Character string. The dependent variable (outcome) for
+#'   logistic regression.
+#' @param predictors Character vector. Independent variables
+#'   (predictors/covariates) for univariable and/or multivariable modelling.
+#' @param ref_level Character string. The factor level of outcome variable that
+#'   corresponds to the true condition (1). If not provided then default is
+#'   `NULL` and the model fit will determine the referenc level.
+#' @param add_multi Logical; include multivariable results. Default is `FALSE`
+#' @param format Display format in case I need to escape some characters. A
+#'   place holder for now in case I need it in the future. Default is "html".
+#' @param conf_level The confidence level to use for the confidence interval.
+#'   Must be strictly greater than 0 and less than 1. Defaults to 0.95, which
+#'   corresponds to a 95 percent confidence interval.
+#' @param exponentiate Logical indicating whether or not to exponentiate the
+#'   the coefficient estimates. This is typical for logistic and multinomial
+#'   regressions, but a bad idea if there is no log or logit link. Defaults to
+#'   `TRUE`.
+#' @param include_last_row Adds a row at the end of each set of results to give
+#'   some breathing room. Default is `TRUE`.
+#'
+#' @importFrom broom tidy
+#' @importFrom car Anova
+#' @importFrom dplyr arrange
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr case_when
+#' @importFrom dplyr coalesce
+#' @importFrom dplyr filter
+#' @importFrom dplyr if_else
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr mutate_at
+#' @importFrom dplyr na_if
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom glue glue
+#' @importFrom janitor clean_names
+#' @importFrom labelled var_label
+#' @importFrom purrr map
+#' @importFrom purrr pluck
+#' @importFrom stats as.formula
+#' @importFrom stats binomial
+#' @importFrom stats drop1
+#' @importFrom stats formula
+#' @importFrom stats glm
+#' @importFrom stringr str_remove
+#' @importFrom tibble as_tibble
+#' @importFrom tibble rownames_to_column
+#' @importFrom tibble tibble
+#' @importFrom tidyr crossing
+#' @importFrom tidyr unnest
+#'
+#' @return A tibble
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' library(epiDisplay)
+#' library(tidyverse)
+#' dplyr::glimpse(infert)
+#'
+#' case ~ induced + spontaneous + education
+#'
+#' display_logistic2(data = infert,
+#'                   outcome = "case",
+#'                   predictors = c("induced", "spontaneous", "education"))
+#'
+#' display_logistic2(data = infert,
+#'                   outcome = "case",
+#'                   predictors = c("induced", "spontaneous", "education"),
+#'                   add_multi = TRUE)
+#'
+#' # How does an interaction term work?
+#' # Doesn't work yet (2019-03-23)
+#' # display_logistic2(data = infert,
+#' #                   outcome = "case",
+#' #                   predictors = c("induced * education"))
+#'
+#' #### Another data set --------------------------------
+#' library(compareGroups)
+#' data(predimed)
+#' dplyr::glimpse(predimed)
+#' predimed <- predimed %>%
+#'   mutate_if(is.double, as.double)
+#'
+#' fit = glm(htn ~ sex + bmi + smoke,
+#'           family = binomial(link = "logit"),
+#'           data = predimed)
+#'
+#' display_logistic2(data = predimed,
+#'                   outcome = "htn",
+#'                   predictors = c("sex", "bmi", "smoke"),
+#'                   add_multi = TRUE)
+#'  }
+
+
+
+
+display_logistic2 <- function(data,
+                              outcome,
+                              predictors,
+                              ref_level = NULL,
+                              add_multi = FALSE,
+                              format = "html",
+                              conf_level = 0.95,
+                              exponentiate = TRUE,
+                              include_last_row = TRUE) {
+
+  # if (length(class(fit)) == 1) {
+  #   stop("Model not from logistic regression")
+  # }
+  # if (class(fit)[1] != "glm" | class(fit)[2] != "lm" |
+  #     fit$family$family != "binomial") {
+  #   stop("Model not from logistic regression")
+  # }
+
+  ## Make the regression formula ----------------
+
+  lhs <- if (!is.null(ref_level)) {
+
+    glue::glue("({outcome} == '{ref_level}')")
+
+  } else {
+
+    glue::glue("{outcome}")
+
+  }
+
+  rhs <- paste(predictors, collapse = " + ")
+
+  form <- glue::glue("{lhs} ~ {rhs}")
+  outcome <- lhs
+
+
+  ## Get the data set ----------------
+
+  # if (add_multi == TRUE) {
+  #
+  #   df <- fit$model
+  #
+  # } else {
+  #
+  #   df <- data
+  #
+  # }
+  # TODO -- not sure about the data set that should be used if doing uni vs
+  # multi. For now just use the full data set.
+  df <- data
+  pred_lvls <- predictors
+
+  ## Univariable results ----------------
+
+  uni_res <- tidyr::crossing(outcome,
+                             predictors) %>%
+    mutate(predictors = factor(predictors,
+                               levels = pred_lvls)) %>%
+    dplyr::arrange(predictors) %>%
+    mutate(predictors = as.character(predictors),
+           formula = paste(outcome, "~", predictors),
+           res_univ =
+             purrr::map(.x = formula,
+                        .f = ~ .do_logistic_univ(data = df,
+                                                 formula = .x,
+                                                 format = format,
+                                                 conf_level = conf_level,
+                                                 exponentiate = exponentiate,
+                                                 include_last_row = include_last_row))) %>%
+    dplyr::select(res_univ) %>%
+    tidyr::unnest()
+
+
+  if (add_multi == TRUE) {
+
+    ## Fit the model ----------------
+
+    fit <- glm(formula = as.formula(form),
+               family = binomial(link = "logit"),
+               data = df)
+
+    ## Multivariable results ----------------
+
+
+    multi_res <- fit %>%
+      broom::tidy(.,
+                  conf.int = TRUE,
+                  conf.level = conf_level,
+                  exponentiate = exponentiate) %>%
+      janitor::clean_names(.) %>%
+      dplyr::select(term,
+                    estimate_adjusted = estimate,
+                    lower_ci_adjusted = conf_low,
+                    upper_ci_adjusted = conf_high,
+                    p_value_wald_adjusted = p_value)
+
+    multi_res_lrt <- car::Anova(fit,
+                                test.statistic = "LR") %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column() %>%
+      tibble::as_tibble(.) %>%
+      janitor::clean_names() %>%
+      dplyr::select(covariate = rowname,
+                    p_value_lrt_adjusted = pr_chisq)
+
+
+    ## Combine to univariable results ----------------
+
+    res <- uni_res %>%
+      mutate(
+        names_to_match = dplyr::na_if(covariate, ""),
+        names_to_match = .repeat_last(names_to_match),
+        names_to_match = paste0(names_to_match, term),
+        names_to_match = dplyr::if_else(estimate == "",
+                                        "",
+                                        names_to_match),
+        names_to_match = trimws(names_to_match, which = "left"),
+        covariate = trimws(covariate, which = "left")) %>%
+      left_join(.,
+                multi_res,
+                by = c("names_to_match" = "term")) %>%
+      dplyr::left_join(.,
+                       multi_res_lrt,
+                       by = "covariate") %>%
+      mutate(signif_wald_adjusted =
+               .calc_sig_ind(p_value_wald_adjusted, format),
+             signif_lrt_adjusted =
+               .calc_sig_ind(p_value_lrt_adjusted, format),
+             signif_adjusted = dplyr::coalesce(signif_wald_adjusted,
+                                               signif_lrt_adjusted)) %>%
+      dplyr::select(-names_to_match,
+                    -signif_wald_adjusted,
+                    -signif_lrt_adjusted) %>%
+      dplyr::rename(estimate_unadjusted = estimate,
+                    lower_ci_unadjusted = lower_ci,
+                    upper_ci_unadjusted = upper_ci,
+                    p_value_wald_unadjusted = p_value_wald,
+                    p_value_lrt_unadjusted = p_value_lrt,
+                    signif_unadjusted = signif)
+
+  } else {
+
+    res <- uni_res
+  }
+
+  res
+
+}
